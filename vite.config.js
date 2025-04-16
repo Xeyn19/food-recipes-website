d102f441f5a079174e88943710d61812b4723c29
Xeyn19 <edgarrodilorosa@gmail.com>
2025-04-16 15:50:07 +0800

    fix for vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  server: {
-    port:3000,
-  }
-})
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      port: 3000,
+    },
+    base: mode === 'production'
+      ? process.env.VITE_BASE_PATH || '/food-recipes-website' 
+      : '/', 
+  };
+});
